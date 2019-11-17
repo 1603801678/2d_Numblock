a40720f8b60265c372e1ac8ec76966f2138441cf
1603801678 <1603801678@qq.com>
2019-11-17 14:07:28 +0800

    Add files via upload

---
+++ java/RUN.bat
@@ -0,0 +1,3 @@
+@echo
+java RunningUI
+pause
--- java/RunningUI.java
+++ java/RunningUI.java
@@ -1,24 +1,25 @@
-import sec.MainGame;
 import sec.GameThread;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 class RunningUI{
 	public static void main(String[] args){
-		Frame main=new Frame("2d_Numblock");
-		main.setLayout(new BorderLayout());
-		main.setBounds(200,200,400,400);
+		JFrame main=new JFrame("2d_Numblock");
+		Box inin=new Box(BoxLayout.Y_AXIS);
+		main.add(inin);
+		main.setBounds(200,200,200,200);
 		Button a=new Button("START");
 		a.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				new GameThread().run();
 			}});
-		main.add(a,BorderLayout.CENTER);
+		inin.add(a);
 		Button b=new Button("QUIT");
 		b.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				System.exit(0);
 			}});
-		main.add(b,BorderLayout.SOUTH);
+		inin.add(b);
 		main.setVisible(true);
 	}
 }
